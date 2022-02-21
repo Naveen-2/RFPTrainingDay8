b831a9a255bccf2a9f72dca884fa54999f72cefb
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-21 19:48:22 +0530

    added UC1 and printed if employee present or absent

--- src/com/EmployeeWage.java
+++ src/com/EmployeeWage.java
@@ -1,4 +1,11 @@
 package com;
 
 public class EmployeeWage {
+    void isPresent(){
+        int isPresent = (int) Math.floor(Math.random() % 2);
+        if(isPresent == 1)
+            System.out.println("Employee Present.");
+        else if (isPresent == 0)
+            System.out.println("Employee Absent.");
+    }
 }

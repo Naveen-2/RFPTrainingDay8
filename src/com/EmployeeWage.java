8ba9914efda98f39a505ba371f114c3cec520390
Naveen-2 <naveen2k15cse@gmail.com>
2022-02-21 21:02:21 +0530

    Added Part time employee wage calculation

--- src/com/EmployeeWage.java
+++ src/com/EmployeeWage.java
@@ -1,24 +1,32 @@
 package com;
 
+
 public class EmployeeWage {
     int dailyWage;
     int fullDayHour = 8;
+    int partTimeHour = 4;
     int wagePerHour = 20;
 
-    boolean isPresent(){
-        int isPresent = (int) Math.floor((Math.random() * 10) % 2);
+    int isPresent(){
+        int isPresent = (int) Math.floor((Math.random() * 10) % 3);
         if(isPresent == 1) {
-            System.out.println("Employee Present.");
-            return true;
+            System.out.println("Employee Present and Full time.");
+            return 1;
+        } else if (isPresent == 2){
+            System.out.println("Employee Present and Part time.");
+            return 2;
         }
         System.out.println("Employee Absent.");
-        return false;
+        return 0;
     }
 
     int dailyWage(){
         dailyWage = 0;
-        if (isPresent()) {
+        int isPresent = isPresent();
+        if (isPresent == 1) {
             dailyWage = wagePerHour * fullDayHour;
+        } else if (isPresent == 2) {
+            dailyWage = wagePerHour * partTimeHour;
         }
         return dailyWage;
     }
--- src/com/Main.java
+++ src/com/Main.java
@@ -3,10 +3,9 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Welcome to Employee Wage Computation Program.");
 
         EmployeeWage employee = new EmployeeWage();
-        System.out.println("Employee salary for today is Rs. " + employee.dailyWage());
+        System.out.println("Salary for the day is Rs. " + employee.dailyWage());
     }
 }
